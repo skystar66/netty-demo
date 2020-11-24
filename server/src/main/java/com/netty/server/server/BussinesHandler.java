30cfb63c673769503282478b9272b241ddbd3ff3
xuliang <xuliang@joyme.sg>
2020-11-24 18:32:18 +0800

    增加线程池处理

--- server/src/main/java/com/netty/server/server/BussinesHandler.java
+++ server/src/main/java/com/netty/server/server/BussinesHandler.java
@@ -11,43 +11,55 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 @Slf4j
 public class BussinesHandler implements RpcAnswer {
 
 
+    ExecutorService executors = Executors.newFixedThreadPool(100);
 
     @Override
     public void callback(RpcCmd rpcCmd) {
-        //获取channel
-        Channel channel = NettyChannelManager.getInstance().getChannel(rpcCmd.getRemoteKey());
-        if (channel == null) {
-            log.info("channel 已失效！");
-            return;
-        }
-        if (Objects.isNull(rpcCmd)) {
-            channel.writeAndFlush(ResponseCode.INVALID_EVENT);
-            log.info("msg is null");
-            return;
-        }
 
-        if (MerticManager.getTcpCount0()==0) {
 
-            log.info("设置 time 时间");
-            MerticManager.time.put("time",System.currentTimeMillis());
-        }
+        executors.execute(new Runnable() {
+            @Override
+            public void run() {
+                //获取channel
+                Channel channel = NettyChannelManager.getInstance().getChannel(rpcCmd.getRemoteKey());
+                if (channel == null) {
+                    log.info("channel 已失效！");
+                    return;
+                }
+                if (Objects.isNull(rpcCmd)) {
+                    channel.writeAndFlush(ResponseCode.INVALID_EVENT);
+                    log.info("msg is null");
+                    return;
+                }
 
-        if (MerticManager.getTcpCount()>=2999) {
+                if (MerticManager.getTcpCount0()==0) {
 
-        log.info("1000条消息，共耗时：{}ms",(System.currentTimeMillis()-MerticManager.time.get("time")));
-            MerticManager.atomicInteger.set(0);
-            MerticManager.atomicInteger1.set(0);
-            MerticManager.time.remove("time");
+                    log.info("设置 time 时间");
+                    MerticManager.time.put("time",System.currentTimeMillis());
+                }
 
-        }
+                if (MerticManager.getTcpCount()>=2999) {
 
-        channel.writeAndFlush(MessageCreator.okResponse(rpcCmd, MessageConstants.STATE_OK));
+                    log.info("1000条消息，共耗时：{}ms",(System.currentTimeMillis()-MerticManager.time.get("time")));
+                    MerticManager.atomicInteger.set(0);
+                    MerticManager.atomicInteger1.set(0);
+                    MerticManager.time.remove("time");
+
+                }
+
+                channel.writeAndFlush(MessageCreator.okResponse(rpcCmd, MessageConstants.STATE_OK));
+
+            }
+        });
+
 
 
 
